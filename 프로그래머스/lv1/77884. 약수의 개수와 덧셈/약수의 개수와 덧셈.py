a16e5f60852ba993e4a3a1a78e8bba501ec368b4
food951 <136784115+food951@users.noreply.github.com>
2023-06-21 09:45:58 +0900

    Update 약수의 개수와 덧셈.py

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,11 +2,11 @@ def solution(left, right):
     answer = 0
     for i in range(left,right+1):
         div = 0
-        for j in range(1,i):
+        for j in range(1,i+1):
             if i%j ==0:
                 div +=1
         if div %2 ==0:
-            answer -=i
+            answer +=i
         else:
-            answer+=i
+            answer -=i
     return answer
