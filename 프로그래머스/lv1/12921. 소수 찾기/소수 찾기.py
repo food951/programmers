209568345d6ec0fd50fc9c599e2071a8889c7536
food951 <136784115+food951@users.noreply.github.com>
2023-06-22 09:12:14 +0900

    [level 1] Title: 소수 찾기, Time: 114.22 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n+1, p):
+                primes[i] = False
+        p += 1
+
+    count = sum(primes)
+    return count
+
