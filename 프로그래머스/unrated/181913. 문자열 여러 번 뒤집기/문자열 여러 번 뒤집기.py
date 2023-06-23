d0c712a1b9d63c9e375ac5c448a80371d20e449d
food951 <136784115+food951@users.noreply.github.com>
2023-06-23 19:01:09 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 1.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,11 +1,11 @@
 def solution(my_string, queries):
     answer = ''
-    b = []
-    list_str=list(my_string)
-    for i in range(len(queries)):
-        b = list_str[queries[i][0]:queries[i][1]+1]
-        b.reverse()
-        list_str[queries[i][0]:queries[i][1]+1] = b
+    list_str=list(my_string) # 문자열을 list 형태로 변환
+    query= []               # 
+    for i in range(len(queries)): 
+        query = list_str[queries[i][0]:queries[i][1]+1] # queries의 배열로 슬라이싱
+        query.reverse()  # 그걸 뒤집음
+        list_str[queries[i][0]:queries[i][1]+1] = query # 뒤집은 query값을 다시 원래 위치에 대체
     
-    answer = "".join(list_str)
+    answer = "".join(list_str) # 리스트를 문자열로 변환
     return answer
