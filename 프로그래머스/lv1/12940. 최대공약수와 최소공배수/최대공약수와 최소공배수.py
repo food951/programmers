2d4254cb9035e68aa18f5006e42ccf928d2545a1
food951 <136784115+food951@users.noreply.github.com>
2023-07-03 09:53:46 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,12 @@
+def solution(n, m):
+    
+    answer = []
+    def gcd(a,b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+    def lcm(a,b):
+        return abs(a * b) // gcd(a, b)
+    answer.append(gcd(n,m))
+    answer.append(lcm(n,m))
+    return answer
