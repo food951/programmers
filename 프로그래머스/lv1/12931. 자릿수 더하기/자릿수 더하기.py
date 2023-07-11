4591a2bd24c3adf5c8be1869497400588e2e8db2
food951 <136784115+food951@users.noreply.github.com>
2023-07-11 13:50:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    str1= str(n)
+    for i in range(len(str1)):
+        answer += int(str1[i])
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return answer
