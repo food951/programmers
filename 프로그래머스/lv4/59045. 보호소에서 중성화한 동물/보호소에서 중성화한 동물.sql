afaff621fd41f751d33ff1963651d62b46ef2149
food951 <136784115+food951@users.noreply.github.com>
2023-09-04 16:40:07 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
-SELECT a.animal_id, a.animal_type,a.name
-from animal_ins as a
-inner join animal_outs as b
-on a.animal_id = b.animal_id
-where (a.SEX_UPON_INTAKE  LIKE 'Intact%') and (b.SEX_UPON_OUTCOME LIKE 'Spayed%' OR b.SEX_UPON_OUTCOME LIKE 'Neutered%')
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE,A.NAME
+FROM ANIMAL_INS A, ANIMAL_OUTS B
+WHERE A.ANIMAL_ID = B.ANIMAL_ID AND 
+    (A.SEX_UPON_INTAKE LIKE 'Intact%' and (b.sex_upon_outcome like 'Spayed%' or b.sex_upon_outcome like 'Neutered%'))
